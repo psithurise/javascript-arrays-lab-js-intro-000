a626562698800476d8888bcf43f18f0113fb2da4
Rose Stough <psithurise@programmer.net>
2019-03-19 21:39:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -32,11 +32,9 @@ function prependKitten(name) {
 }
 
 function removeLastKitten() {
-  kittens.slice(0, kittens.length-1)
-  return kittens
+  return kittens.slice(0, kittens.length-1)
 }
 
 function removeFirstKitten() {
-  kittens.slice(0)
-  return kittens
+  return kittens.slice(0)
 }
